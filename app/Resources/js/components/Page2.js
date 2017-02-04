af29d46d39c988add1eaec2807a0fbeba5d78afa
Anthony Tarlao <anthony.tarlao@gmail.com>
2017-02-04 01:17:27 +0100

    small fix

--- app/Resources/js/components/Page2.js
+++ app/Resources/js/components/Page2.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Page2 = () => {
     return (
         <div>
-            <h1>Titre page 1</h1>
-            <div>Contenu page 1</div>
+            <h1>Titre page 2</h1>
+            <div>Contenu page 2</div>
         </div>
     );
 };
